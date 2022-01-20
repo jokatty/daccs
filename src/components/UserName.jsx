d7454fa61a8cdcddf681112c31f0652fb11568c4
jokatty <jyotikattani@gmail.com>
2022-01-20 12:07:05 +0200

    create user name header for  user information

--- src/components/UserName.jsx
+++ src/components/UserName.jsx
@@ -3,11 +3,13 @@ import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import './UserName.css';
 
 export default function UserName() {
   return (
     <>
-      <div>
+      <div className="userName">
         <Stack direction="row" spacing={2}>
           <Avatar>JD</Avatar>
         </Stack>
@@ -17,16 +19,16 @@ export default function UserName() {
           </Grid>
           <Grid xs={6}>
             <Grid xs={12}>
+              <Typography>Name</Typography>
+            </Grid>
+            <Grid xs={12}>
               <Typography variant="h6" component="h6">
                 Justus Darley
               </Typography>
             </Grid>
-            <Grid xs={12}>
-              <p>Name</p>
-            </Grid>
           </Grid>
           <Grid item xs={3}>
-            <p>2</p>
+            <MoreHorizIcon />
           </Grid>
         </Grid>
       </div>
