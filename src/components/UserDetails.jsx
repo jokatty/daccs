10ad2525e109e69318663d46aea5d2a3fc0c22b3
jokatty <jyotikattani@gmail.com>
2022-01-25 15:42:12 +0100

    display user data

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,23 +1,32 @@
 import { Grid } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './UserDetails.css';
+import getUserData from '../store';
 
 export default function UserDetails() {
+  // set the initial state
+  const [userDetails, setUserDetails] = useState({ user_address: '', bank_account: '', email: '' });
+  useEffect(async () => {
+    const userData = await getUserData();
+    console.log(userData);
+    setUserDetails(userData);
+  }, []);
+
   return (
     <>
       <div className="userDetailsDiv">
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           <Grid xs={6}>
-            <div>Address</div>
+            <div>{userDetails.user_address}</div>
           </Grid>
           <Grid xs={6}>
-            <div>IBAN Bank Account</div>
+            <div>{userDetails.bank_account}</div>
           </Grid>
           <Grid xs={6}>
-            <div>Email</div>
+            <div>{userDetails.email}</div>
           </Grid>
           <Grid xs={6}>
-            <div>Phone Number</div>
+            <div>{userDetails.phone}</div>
           </Grid>
         </Grid>
       </div>
