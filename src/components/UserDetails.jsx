793b0eef20b8fb520f842ff5efd42d79a1a9d7da
jokatty <jyotikattani@gmail.com>
2022-03-16 16:50:36 +0100

    style user details component

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,28 +1,74 @@
-import { Grid } from '@mui/material';
+import { Grid, Divider, Typography } from '@mui/material';
 import React from 'react';
 import './UserDetails.css';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import LinkOutlinedIcon from '@mui/icons-material/LinkOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 
 export default function UserDetails(prop) {
   const { userDetails } = prop;
+  const stylingObj = {
+    container: {
+      padding: '1.5rem',
+    },
+    fieldColor: {
+      color: 'gray',
+      fontSize: '.65rem',
+    },
+    fieldValue: {
+      color: 'black',
+      fontSize: '.75rem',
+    },
+    alignment: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+
+  };
   return (
     <>
-      <div className="userDetailsDiv">
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid xs={6}>
-            <div>{userDetails.user_address}</div>
-          </Grid>
-          <Grid xs={6}>
-            <div>{userDetails.bank_account}</div>
-          </Grid>
-          <Grid xs={6}>
-            <div>{userDetails.email}</div>
-          </Grid>
-          <Grid xs={6}>
-            <div>{userDetails.phone}</div>
+      <Grid container style={stylingObj.container}>
+        <Grid xs={1} style={stylingObj.alignment}><LocationOnOutlinedIcon fontSize="small" color="disabled" /></Grid>
+        <Grid xs={5}>
+          <Grid container sx={{ flexDirection: 'column' }}>
+            <Grid>
+              <Typography variant="subtitle2" style={stylingObj.fieldColor}>Address</Typography>
+            </Grid>
+            <Grid>
+              <Typography variant="subtitle1" style={stylingObj.fieldValue} mt={-0.5}>{userDetails.user_address}</Typography>
+            </Grid>
           </Grid>
         </Grid>
-      </div>
-      <hr />
+        <Grid xs={1} style={stylingObj.alignment}><LinkOutlinedIcon fontSize="small" color="disabled" /></Grid>
+        <Grid xs={5}>
+          <Grid container sx={{ flexDirection: 'column' }}>
+            <Grid><Typography variant="subtitle2" style={stylingObj.fieldColor}>IBAN bank account</Typography></Grid>
+          </Grid>
+          <Grid>
+            <Typography variant="subtitle1" style={stylingObj.fieldValue} mt={-0.5}>{userDetails.bank_account}</Typography>
+          </Grid>
+        </Grid>
+        <Grid container mt={2}>
+          <Grid xs={1} style={stylingObj.alignment}><EmailOutlinedIcon fontSize="small" color="disabled" /></Grid>
+          <Grid xs={5}>
+            <Grid container sx={{ flexDirection: 'column' }}>
+              <Grid><Typography variant="subtitle2" style={stylingObj.fieldColor}>Email address</Typography></Grid>
+              <Grid>
+                <Typography variant="subtitle1" style={stylingObj.fieldValue} mt={-0.5}>{userDetails.email}</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={1} style={stylingObj.alignment}><LocalPhoneOutlinedIcon fontSize="small" color="disabled" /></Grid>
+          <Grid xs={5}>
+            <Grid container sx={{ flexDirection: 'column' }}>
+              <Grid><Typography variant="subtitle2" style={stylingObj.fieldColor}>Phone number</Typography></Grid>
+              <Grid><Typography variant="subtitle1" style={stylingObj.fieldValue} mt={-0.5}>{userDetails.phone}</Typography></Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider />
     </>
   );
 }
